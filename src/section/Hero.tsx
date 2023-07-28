a029e03cd0d33684a283e52b33a4af58fb25cc62
ImAdityaa12 <117254771+ImAdityaa12@users.noreply.github.com>
2023-07-28 22:30:10 +0530

    Updated Hero.tsx

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -59,7 +59,7 @@ const Hero = () => {
           delay: 1.65,
         }}
         >
-            <Button text='Download CV' link='https://aditya-portfolio-nextjs.netlify.app/resume.pdf'/>
+            <Button text='Download CV' link='https://aditya-portfolio-nextjs.netlify.app/CV.pdf'/>
             
         </motion.div>
     </div>
